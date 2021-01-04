71bac5a7a9cf8e6bfb9b5a0514d0c49a813cdf0a
bdotbull <itsbrandonbull@gmail.com>
2021-01-04 14:53:43 -0600

    just starting

---
+++ rlah/src/rlah.py
@@ -0,0 +1,23 @@
+'''
+Me: Can we get Rocket League?
+Them: No, we have Rocket League at home.
+
+Rocket League at home:
++------+
+|  O   |
+|      |
+|     X|
++------+
+'''
+
+# Variables
+field_width = 3
+field_height = 3
+
+ballX = 1
+ballY = 1
+ball = ' 0 '
+
+playerX = 2
+playerY = 2
+player = '|X|'
