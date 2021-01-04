0414a7436d062436748d8d3a3412eee0eec53046
bdotbull <itsbrandonbull@gmail.com>
2021-01-04 09:54:09 -0600

    added atba - Bullish

---
+++ Bullish/Bullish.py
@@ -0,0 +1,25 @@
+from tools import  *
+from objects import *
+from routines import *
+
+
+#This file is for strategy
+
+class BullBot(GoslingAgent):
+    def run(agent):
+        #An example of using raw utilities:
+        relative_target = agent.ball.location - agent.me.location
+        local_target = agent.me.local(relative_target)  # convert into local coords
+        
+        # once we have it in local coords, we can pass it to the two main functions.
+        defaultPD(agent, local_target) #
+        defaultThrottle(agent, 2300)  # gets car to certain speed (max==2300)
+
+        #An example of pushing routines to the stack:
+        if len(agent.stack) < 1:
+            if agent.kickoff_flag:
+                agent.push(kickoff())
+            else:
+                agent.push(atba())  # basically the same thing as the 
+                                    # example using raw utils, seen above
+
