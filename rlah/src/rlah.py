0211e1db9c7498d54dacf31b69422fc1b8f66625
bdotbull <itsbrandonbull@gmail.com>
2021-01-04 16:39:41 -0600

    outlining functions

--- rlah/src/rlah.py
+++ rlah/src/rlah.py
@@ -3,12 +3,15 @@ Me: Can we get Rocket League?
 Them: No, we have Rocket League at home.
 
 Rocket League at home:
-+------+
-|  O   |
-|      |
-|     X|
-+------+
 '''
+
+MAP = [
+    "+-G-+",
+    "|   |",
+    "|   |",
+    "|   |",
+    "+-g-+"
+]
 
 # Variables
 field_width = 3
@@ -16,8 +19,30 @@ field_height = 3
 
 ballX = 1
 ballY = 1
-ball = ' 0 '
+ball = '0'
 
 playerX = 2
 playerY = 2
-player = '|X|'
+player = 'X'
+
+# Functions
+def render():
+    pass
+
+def move_player():
+    pass
+
+def move_ball():
+    '''the ball is against a wall and is hit towards the same wall, 
+        the ball and player swap positions.  This is as if you slam 
+        the ball into the wall and it bounces behind you
+    '''
+    pass
+
+def win():
+    '''If the ball is in the middle of the upper row and pushed north, we win!
+    '''
+    pass
+
+def lose():
+    pass
