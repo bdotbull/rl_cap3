6d77f0eb1c15b3f11a7d701938dcf1875496609e
bdotbull <itsbrandonbull@gmail.com>
2021-01-04 10:18:38 -0600

    Bullish, the Bully (chases opponent)

--- Bullish/Bullish.py
+++ Bullish/Bullish.py
@@ -7,14 +7,14 @@ from routines import *
 
 class BullBot(GoslingAgent):
     def run(agent):
-        #An example of using raw utilities:
-        relative_target = agent.ball.location - agent.me.location
+        # Using raw utilities:
+        relative_target = agent.foes[0].location - agent.me.location # chases opponent
         local_target = agent.me.local(relative_target)  # convert into local coords
         
         # once we have it in local coords, we can pass it to the two main functions.
         defaultPD(agent, local_target) #
         defaultThrottle(agent, 2300)  # gets car to certain speed (max==2300)
-
+'''
         #An example of pushing routines to the stack:
         if len(agent.stack) < 1:
             if agent.kickoff_flag:
@@ -22,4 +22,4 @@ class BullBot(GoslingAgent):
             else:
                 agent.push(atba())  # basically the same thing as the 
                                     # example using raw utils, seen above
-
+'''
