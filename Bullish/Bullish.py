c5f9b6c4c325f3d453b5a56fca341a0ca3b3e3e5
bdotbull <itsbrandonbull@gmail.com>
2021-01-04 14:17:45 -0600

    atba using stack

--- Bullish/Bullish.py
+++ Bullish/Bullish.py
@@ -7,6 +7,7 @@ from routines import *
 
 class BullBot(GoslingAgent):
     def run(agent):
+        '''
         # Using raw utilities:
         relative_target = agent.foes[0].location - agent.me.location # chases opponent
         local_target = agent.me.local(relative_target)  # convert into local coords
@@ -14,12 +15,11 @@ class BullBot(GoslingAgent):
         # once we have it in local coords, we can pass it to the two main functions.
         defaultPD(agent, local_target) #
         defaultThrottle(agent, 2300)  # gets car to certain speed (max==2300)
-'''
-        #An example of pushing routines to the stack:
+        
+        '''
+        # atba using the stack:
         if len(agent.stack) < 1:
             if agent.kickoff_flag:
                 agent.push(kickoff())
             else:
-                agent.push(atba())  # basically the same thing as the 
-                                    # example using raw utils, seen above
-'''
+                agent.push(atba())
